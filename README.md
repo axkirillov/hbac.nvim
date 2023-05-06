6916ceb6f2cf102478940f8f3092ddf6bffce3f1
ditsuke <ditsuke@protonmail.com>
2023-05-06 23:34:55 +0530

    feat(autoclose): Allow toggling with a command
    - Adds a command to toggle auto close.
    - Makes autoclose able to close multiple buffers in one
      trigger, which is majorly a necessity born out of the
      toggling feature that would let someone open >threshold
      buffers and then enable autoclose, which would mean
      the expected behavior is closing buffers until
      open buffers <= threshold or closable (non-pinned, live)
      buffers are exhausted.
    - docs: Update README

--- README.md
+++ README.md
@@ -29,6 +29,10 @@ with lazy
 require("hbac").setup({
   autoclose = true, -- set autoclose to false if you want to close manually
   threshold = 10 -- hbac will start closing unedited buffers once that number is reached
+  close_command = function(bufnr)
+    vim.api.nvim_buf_delete(bufnr, {})
+  end,
+  close_buffers_with_windows = false, -- hbac will close buffers with associated windows if this option is `true`
 })
 ```
 
@@ -39,10 +43,12 @@ or
 
 - `:Hbac toggle_pin` - toggle a pin of the current buffer to prevent it from being auto-closed
 - `:Hbac close_unpinned` - close all unedited/unpinned buffers
+- `:Hbac toggle_autoclose` - toggle autoclose behavior
 
 or, if you prefer to use lua:
 
 ```lua
 require("hbac").toggle_pin()
 require("hbac").close_unpinned()
+require("hbac").toggle_autoclose()
 ```
--- lua/hbac/setup.lua
+++ lua/hbac/setup.lua
@@ -1,18 +1,74 @@
 local command = require("hbac.command")
-local state   = require("hbac.state")
+local state = require("hbac.state")
 
-local M = {}
+local M = {
+	CONSTANTS = {
+		AUGROUP_AUTO_CLOSE = "hbac_autoclose",
+	},
+	opts = {
+		threshold = 10,
+		autoclose = true,
+	},
+}
 
 local id = vim.api.nvim_create_augroup("hbac", {
-	clear = false
+	clear = false,
 })
 
-M.setup = function(opts)
-	opts = vim.tbl_extend("force", {
-		autoclose = true,
-		threshold = 10,
-	}, opts or {})
+M.setup_autoclose = function()
+	state.autoclose_enabled = true
+	vim.api.nvim_create_autocmd({ "BufEnter" }, {
+		group = vim.api.nvim_create_augroup(M.CONSTANTS.AUGROUP_AUTO_CLOSE, { clear = true }),
+		pattern = { "*" },
+		callback = function()
+			local current_buf = vim.api.nvim_get_current_buf()
+			local buftype = vim.api.nvim_buf_get_option(current_buf, "buftype")
+			-- if the buffer is not a file - do nothing
+			if buftype ~= "" then
+				return
+			end
 
+			local buffers = vim.tbl_filter(function(buf)
+				local name = vim.api.nvim_buf_get_name(buf)
+				local listed = vim.api.nvim_buf_get_option(buf, "buflisted")
+				if name ~= "" and listed then
+					return true
+				end
+				return false
+			end, vim.api.nvim_list_bufs())
+			local num_buffers = #buffers
+			if num_buffers <= M.opts.threshold then
+				return
+			end
+
+			local buffers_to_close = num_buffers - M.opts.threshold
+
+			-- Buffer sorted by current > pinned > others (by bufnr)
+			table.sort(buffers, function(a, b)
+				if a == current_buf or state.is_pinned(a) then
+					return false
+				end
+				if b == current_buf or state.is_pinned(b) then
+					return true
+				end
+				return a < b
+			end)
+
+			for i = 1, buffers_to_close, 1 do
+				local buffer = buffers[i]
+				if not state.is_pinned(buffer) and buffer ~= current_buf then
+					vim.api.nvim_buf_delete(buffer, {})
+				else -- We've reached the pinned buffers/current buffer
+					break
+				end
+			end
+		end,
+	})
+end
+
+M.setup = function(opts)
+	M.opts = vim.tbl_extend("force", M.opts, opts or {})
+	opts = M.opts
 	vim.api.nvim_create_autocmd({ "BufRead" }, {
 		group = id,
 		pattern = { "*" },
@@ -26,64 +82,20 @@ M.setup = function(opts)
 						return
 					end
 					state.toggle_pin(bufnr)
-				end
+				end,
 			})
-		end
+		end,
 	})
 
-	vim.api.nvim_create_user_command(
-		command.vim_cmd_name,
-		function(args)
-			command.vim_cmd_func(args.args)
-		end,
-		command.vim_cmd_opts
-	)
+	vim.api.nvim_create_user_command(command.vim_cmd_name, function(args)
+		command.vim_cmd_func(args.args)
+	end, command.vim_cmd_opts)
 
 	if not opts.autoclose then
 		return
 	end
 
-	vim.api.nvim_create_autocmd({ "BufEnter" }, {
-		group = id,
-		pattern = { "*" },
-		callback = function()
-			local bufnr = vim.api.nvim_get_current_buf()
-			local buftype = vim.api.nvim_buf_get_option(bufnr, 'buftype')
-			-- if the buffer is not a file - do nothing
-			if buftype ~= "" then
-				return
-			end
-
-			local min = 2 ^ 1023
-			local buffers = vim.api.nvim_list_bufs()
-			local num_buffers = 0
-			for _, buf in ipairs(buffers) do
-				local name = vim.api.nvim_buf_get_name(buf)
-				local listed = vim.api.nvim_buf_get_option(buf, 'buflisted')
-				if name ~= "" and listed then
-					num_buffers = num_buffers + 1
-					if not state.is_pinned(buf) then
-						min = math.min(min, buf)
-					end
-				end
-			end
-
-			if num_buffers <= opts.threshold then
-				return
-			end
-
-			if min == bufnr then
-				return
-			end
-
-			if min >= 2 ^ 1023 then
-				return
-			end
-
-			vim.api.nvim_buf_delete(min, {})
-		end
-	})
-
+	M.setup_autoclose()
 end
 
 return M
