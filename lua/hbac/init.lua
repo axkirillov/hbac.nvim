ba4a607d9ea9c58a0add0d2875e6aa37023a4cd3
ditsuke <ditsuke@protonmail.com>
2023-05-08 15:05:14 +0530

    refactor: Factor out autocommand setup to new mod
    Also some features:
    - Improved close-prioritising heuristic (incl some bug fixes)
    - Configure to use custom close command (for example, `mini.bufremove`)
    
    chore: Suggestion from code review
    
    chore: Suggestions from code review

--- lua/hbac/init.lua
+++ lua/hbac/init.lua
@@ -4,5 +4,6 @@ return {
 	setup = require("hbac.setup").setup,
 	cmd = command.vim_cmd_func,
 	close_unpinned = command.subcommands.close_unpinned,
-	toggle_pin = command.subcommands.toggle_pin
+	toggle_pin = command.subcommands.toggle_pin,
+	toggle_autoclose = command.subcommands.toggle_autoclose,
 }
--- lua/hbac/setup.lua
+++ lua/hbac/setup.lua
@@ -1,13 +1,13 @@
-local command = require("hbac.command")
 local state = require("hbac.state")
 
 local M = {
-	CONSTANTS = {
-		AUGROUP_AUTO_CLOSE = "hbac_autoclose",
-	},
 	opts = {
 		threshold = 10,
 		autoclose = true,
+		close_buffers_with_windows = false,
+		close_command = function(bufnr)
+			vim.api.nvim_buf_delete(bufnr, {})
+		end,
 	},
 }
 
@@ -15,61 +15,10 @@ local id = vim.api.nvim_create_augroup("hbac", {
 	clear = false,
 })
 
-M.setup_autoclose = function()
-	state.autoclose_enabled = true
-	vim.api.nvim_create_autocmd({ "BufEnter" }, {
-		group = vim.api.nvim_create_augroup(M.CONSTANTS.AUGROUP_AUTO_CLOSE, { clear = true }),
-		pattern = { "*" },
-		callback = function()
-			local current_buf = vim.api.nvim_get_current_buf()
-			local buftype = vim.api.nvim_buf_get_option(current_buf, "buftype")
-			-- if the buffer is not a file - do nothing
-			if buftype ~= "" then
-				return
-			end
+M.setup = function(user_opts)
+	local command = require("hbac.command")
+	M.opts = vim.tbl_extend("force", M.opts, user_opts or {})
 
-			local buffers = vim.tbl_filter(function(buf)
-				local name = vim.api.nvim_buf_get_name(buf)
-				local listed = vim.api.nvim_buf_get_option(buf, "buflisted")
-				local windows_with_buf = vim.fn.win_findbuf(buf)
-				if name ~= "" and listed and #windows_with_buf == 0 then
-					return true
-				end
-				return false
-			end, vim.api.nvim_list_bufs())
-			local num_buffers = #buffers
-			if num_buffers <= M.opts.threshold then
-				return
-			end
-
-			local buffers_to_close = num_buffers - M.opts.threshold
-
-			-- Buffer sorted by current > pinned > others (by bufnr)
-			table.sort(buffers, function(a, b)
-				if a == current_buf or state.is_pinned(a) then
-					return false
-				end
-				if b == current_buf or state.is_pinned(b) then
-					return true
-				end
-				return a < b
-			end)
-
-			for i = 1, buffers_to_close, 1 do
-				local buffer = buffers[i]
-				if not state.is_pinned(buffer) and buffer ~= current_buf then
-					vim.api.nvim_buf_delete(buffer, {})
-				else -- We've reached the pinned buffers/current buffer
-					break
-				end
-			end
-		end,
-	})
-end
-
-M.setup = function(opts)
-	M.opts = vim.tbl_extend("force", M.opts, opts or {})
-	opts = M.opts
 	vim.api.nvim_create_autocmd({ "BufRead" }, {
 		group = id,
 		pattern = { "*" },
@@ -92,11 +41,9 @@ M.setup = function(opts)
 		command.vim_cmd_func(args.args)
 	end, command.vim_cmd_opts)
 
-	if not opts.autoclose then
-		return
+	if M.opts.autoclose then
+		require("hbac.autocommands").autoclose.setup()
 	end
-
-	M.setup_autoclose()
 end
 
 return M
